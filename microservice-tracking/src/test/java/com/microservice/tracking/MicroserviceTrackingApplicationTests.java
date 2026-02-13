67193d0d387e5d6a02a0e3f4fb0eedce13dc7f75
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-13 21:10:04 +0100

    create project traking service

---
+++ microservice-tracking/src/test/java/com/microservice/tracking/MicroserviceTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
