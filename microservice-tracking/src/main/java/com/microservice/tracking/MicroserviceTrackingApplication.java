7f7b8274db2b0c917e2ce57689454d890ab2f388
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-14 21:28:50 +0100

    add mongodb in docker compose file

--- microservice-tracking/src/main/java/com/microservice/tracking/MicroserviceTrackingApplication.java
+++ microservice-tracking/src/main/java/com/microservice/tracking/MicroserviceTrackingApplication.java
@@ -2,16 +2,10 @@ package com.microservice.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = { 
-    DataSourceAutoConfiguration.class, 
-    DataSourceTransactionManagerAutoConfiguration.class, 
-    HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class MicroserviceTrackingApplication {
 
 	public static void main(String[] args) {
