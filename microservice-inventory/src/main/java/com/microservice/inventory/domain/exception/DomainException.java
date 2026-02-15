d6ae907e112ff1b69970d08bf8401da2e5b0ccb2
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-15 20:01:03 +0100

    add DomainException

---
+++ microservice-inventory/src/main/java/com/microservice/inventory/domain/exception/DomainException.java
@@ -0,0 +1,9 @@
+package com.microservice.inventory.domain.exception;
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+}
