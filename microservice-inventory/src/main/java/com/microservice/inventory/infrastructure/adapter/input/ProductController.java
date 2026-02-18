0f0ce34d36a67b72f307780725a0cd2448197318
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-18 20:34:25 +0100

    create getByProductId controller

--- microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/input/ProductController.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/input/ProductController.java
@@ -1,13 +1,27 @@
 package com.microservice.inventory.infrastructure.adapter.input;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservice.inventory.application.dto.ProductReponseDto;
+import com.microservice.inventory.application.mapper.ProductDtoMapper;
+import com.microservice.inventory.application.port.input.GetProductDetailsUseCase;
+import com.microservice.inventory.domain.model.ProductDetails;
+
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    private final GetProductDetailsUseCase getProductDetailsUseCase;
+    private final ProductDtoMapper productDtoMapper;
+
+    public ProductController(GetProductDetailsUseCase getProductDetailsUseCase, ProductDtoMapper productDtoMapper) {
+        this.getProductDetailsUseCase = getProductDetailsUseCase;
+        this.productDtoMapper = productDtoMapper;
+    }
 
     @GetMapping()
     public String getProducts() {
@@ -15,8 +29,13 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public String getProductById(@RequestParam("id") String id) {
-        return "Product details for ID: " + id;
+    public ResponseEntity<ProductReponseDto> getProductById(@PathVariable Long id) {
+    
+        ProductDetails details = getProductDetailsUseCase.getProductDetails(id);
+    
+        ProductReponseDto response = productDtoMapper.toResponse(details);
+        
+        return ResponseEntity.ok(response);
     }
 
 }
