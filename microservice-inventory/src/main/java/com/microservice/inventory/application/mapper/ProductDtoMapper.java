a79b8f27bf9b54d3b11671f281c20b7a85bab37f
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-21 18:44:24 +0100

    add method to entity ProductDtoMapper

--- microservice-inventory/src/main/java/com/microservice/inventory/application/mapper/ProductDtoMapper.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/mapper/ProductDtoMapper.java
@@ -3,10 +3,12 @@ package com.microservice.inventory.application.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.microservice.inventory.application.dto.ProductCreateDto;
 import com.microservice.inventory.application.dto.ProductReponseDto;
 import com.microservice.inventory.domain.model.ProductDetails;
+import com.microservice.inventory.infrastructure.adapter.output.persistence.Product.ProductEntity;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { StockDtoMapper.class })
 public interface ProductDtoMapper {
 
     @Mapping(target = "id", source = "product.id")
@@ -14,14 +16,16 @@ public interface ProductDtoMapper {
     @Mapping(target = "name", source = "product.name")
     @Mapping(target = "description", source = "product.description")
     @Mapping(target = "price", source = "product.price.amount") // Extrae del Value Object
-    
     //@Mapping(target = "availableStock", expression = "java(details.stock() != null ? details.stock().available() : 0)")
     @Mapping(target = "availableStock", source = "details") // Mapear directamente el objeto ProductDetails
     ProductReponseDto toResponse(ProductDetails details);
 
+    @Mapping(target = "id", ignore = true)
+    ProductEntity toEntity(ProductCreateDto createDto);
+
     //Si el stock existe, llama al método de negocio available() que se definio en la clase Stock
     default Integer mapAvailableStock(ProductDetails details) {
-    if (details.stock() == null) return 0;
-    return details.stock().available();
-}
+        if (details.stock() == null) return 0;
+        return details.stock().available();
+    }
 }
