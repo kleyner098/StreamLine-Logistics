c9649cde3b83f8f051ea10ad023fb8e8730fa385
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-22 21:19:29 +0100

    add validation

--- microservice-inventory/src/main/java/com/microservice/inventory/application/dto/ProductCreateDto.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/dto/ProductCreateDto.java
@@ -2,6 +2,11 @@ package com.microservice.inventory.application.dto;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +18,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProductCreateDto {
 
+    @NotBlank
     private String sku;
+
+    @NotBlank
     private String name;
+
     private String description;
+
+    @NotNull
+    @DecimalMin(value = "0.0")
     private BigDecimal price;
-    private int stock;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer stock;
 }
