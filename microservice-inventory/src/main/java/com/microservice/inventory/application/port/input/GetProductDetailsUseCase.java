8db79e973d10417b41f0ec9b00f6af532439024a
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-26 10:51:39 +0100

    add getall method GetProductUseCase

--- microservice-inventory/src/main/java/com/microservice/inventory/application/port/input/GetProductDetailsUseCase.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/port/input/GetProductDetailsUseCase.java
@@ -1,8 +1,11 @@
 package com.microservice.inventory.application.port.input;
 
+import java.util.ArrayList;
+
 import com.microservice.inventory.domain.model.ProductDetails;
 
 public interface GetProductDetailsUseCase {
 
     public ProductDetails getProductDetails(Long productId);
+    public ArrayList<ProductDetails> getAllProducts();
 }
