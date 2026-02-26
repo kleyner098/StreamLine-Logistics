df7b04e8ee819e69eb1bae83f272e303f330f97d
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-26 10:52:11 +0100

    add findAll method to ProductRepository interface

--- microservice-inventory/src/main/java/com/microservice/inventory/application/port/output/ProductRepository.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/port/output/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.microservice.inventory.application.port.output;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.microservice.inventory.domain.model.Product;
 
 public interface ProductRepository {
+
+    List<Product> findAll();
     Optional<Product> findById(Long id);
     Optional<Product> findBySku(String sku);
     Product save(Product product);
+    
 }
