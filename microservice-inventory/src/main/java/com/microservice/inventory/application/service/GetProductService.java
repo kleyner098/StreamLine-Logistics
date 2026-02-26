05203cfe4c3b4c1cd265ff03af9db09a64ef9433
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-26 10:36:43 +0100

    refacto GetProdcutService

--- microservice-inventory/src/main/java/com/microservice/inventory/application/service/GetProductService.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/service/GetProductService.java
@@ -1,30 +1,30 @@
 package com.microservice.inventory.application.service;
 
+import com.microservice.inventory.application.exception.ApplicationException;
 import com.microservice.inventory.application.port.input.GetProductDetailsUseCase;
 import com.microservice.inventory.application.port.output.ProductRepository;
 import com.microservice.inventory.application.port.output.StockRepository;
-import com.microservice.inventory.domain.exception.DomainException;
 import com.microservice.inventory.domain.model.Product;
 import com.microservice.inventory.domain.model.ProductDetails;
 import com.microservice.inventory.domain.model.Stock;
 
 public class GetProductService implements GetProductDetailsUseCase{
 
-    private final ProductRepository productRepositoy;
+    private final ProductRepository productRepository;
     private final StockRepository stockRepository;
 
-    public GetProductService(ProductRepository productRepositoy, StockRepository stockRepository) {
-        this.productRepositoy = productRepositoy;
+    public GetProductService(ProductRepository productRepository, StockRepository stockRepository) {
+        this.productRepository = productRepository;
         this.stockRepository = stockRepository;
     }
 
     @Override
     public ProductDetails getProductDetails(Long productId) {
-        Product product = productRepositoy.findById(productId)
-            .orElseThrow(() -> new DomainException("Producto con ID " + productId + " no encontrado"));
+        Product product = productRepository.findById(productId)
+            .orElseThrow(() -> new ApplicationException("Producto con ID " + productId + " no encontrado"));
 
         Stock stock = stockRepository.findByProductId(productId)
-            .orElseThrow(() -> new DomainException("Stock no disponible para el producto con ID " + productId));
+            .orElseThrow(() -> new ApplicationException("Stock no disponible para el producto con ID " + productId));
         
         return new ProductDetails(product, stock);
     }
