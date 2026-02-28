d3e65f0b997a6d68bc716debfaa5a3d7f0b20c71
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-28 12:20:46 +0100

    fix: enhance exception handling in PriceTest for negative and null values

--- microservice-inventory/src/test/java/com/microservice/inventory/domain/valueobject/PriceTest.java
+++ microservice-inventory/src/test/java/com/microservice/inventory/domain/valueobject/PriceTest.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.microservice.inventory.domain.exception.DomainException;
 
@@ -20,14 +21,24 @@ public class PriceTest {
     @Test
     @DisplayName("Crear un precio con un valor negativo")
     void createPriceWithNegativeValue() {
-        assertThrows(DomainException.class, 
+        DomainException exception = assertThrows(DomainException.class, 
             () -> new Price(new BigDecimal("-1.0")));   
+        assertTrue(exception.getMessage().equals("El precio no puede ser nulo o 0"));
     }
 
     @Test
     @DisplayName("Crear un precio con un valor nulo")
     void createPriceWithNullValue() {
-        assertThrows(DomainException.class,
+        DomainException exception = assertThrows(DomainException.class,
             () -> new Price(null));
+        assertTrue(exception.getMessage().equals("El precio no puede ser nulo o 0"));
+    }
+
+    @Test
+    @DisplayName("Crear un precio con más de 2 decimales")
+    void createPriceWithMoreThanTwoDecimals() {
+        DomainException exception = assertThrows(DomainException.class,
+            () -> new Price(new BigDecimal("10.999")));
+        assertTrue(exception.getMessage().equals("El precio no puede tener más de 2 decimales"));
     }
 }
