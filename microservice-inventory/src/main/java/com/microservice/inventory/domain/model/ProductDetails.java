7dfbb4009afa1a6c20506378b3a89951637b4f66
kleyner098 <kleyner_ecua@hotmail.com>
2026-02-28 11:43:15 +0100

    fix: update isOutOfStock method to correctly check stock availability

--- microservice-inventory/src/main/java/com/microservice/inventory/domain/model/ProductDetails.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/domain/model/ProductDetails.java
@@ -4,6 +4,6 @@ public record ProductDetails(Product product,
     Stock stock
 ) {
     public boolean isOutOfStock() {
-        return stock == null || stock == null;
+        return stock == null || stock.available() <= 0;
     }
 }
