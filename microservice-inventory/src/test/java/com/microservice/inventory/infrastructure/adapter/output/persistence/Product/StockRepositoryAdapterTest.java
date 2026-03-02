291cf670e19139a9821372e9afdae2336bef58eb
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-02 19:55:51 +0100

    test: add StockRepositoryAdapterTest to validate stock persistence and retrieval

---
+++ microservice-inventory/src/test/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Product/StockRepositoryAdapterTest.java
@@ -0,0 +1,70 @@
+package com.microservice.inventory.infrastructure.adapter.output.persistence.Product;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.microservice.inventory.domain.model.Stock;
+import com.microservice.inventory.infrastructure.adapter.output.persistence.Stock.JpaStockRepository;
+import com.microservice.inventory.infrastructure.adapter.output.persistence.Stock.StockRepositoryAdapter;
+import com.microservice.inventory.infrastructure.adapter.output.persistence.mappers.StockEntityMapperImpl;
+import com.microservice.inventory.infrastructure.shared.BaseRepositoryTest;
+
+public class StockRepositoryAdapterTest extends BaseRepositoryTest {
+
+    @Autowired
+    private JpaStockRepository jpaStockRepository;
+
+    private StockEntityMapperImpl stockEntityMapper;
+
+    private StockRepositoryAdapter stockRepositoryAdapter;
+
+    private Long productId;
+
+    @BeforeEach
+    void setup() {
+        stockEntityMapper = new StockEntityMapperImpl();
+        stockRepositoryAdapter = new StockRepositoryAdapter(jpaStockRepository, stockEntityMapper);
+
+        
+        // Limpiar la base de datos antes de cada prueba
+        jpaStockRepository.deleteAll(); 
+
+        productId = 1L;
+
+        //Guardar un stock de prueba en la base de datos
+        Stock stock = new Stock(2L, productId, 100, 0);
+        stockRepositoryAdapter.save(productId, stock);
+    }
+
+
+    @Test
+    @DisplayName("Guardar un stock en la base de datos")
+    void saveStockInDatabase(){
+
+        //Given
+        Stock newstock = new Stock(2L,2L, 50, 0);
+
+        //When
+        Stock savedStockEntity = stockRepositoryAdapter.save(2L, newstock);
+
+        //then
+        assertNotNull(savedStockEntity.getId());
+        assertNotNull(jpaStockRepository.findById(savedStockEntity.getId()));
+    }
+
+    @Test
+    @DisplayName("Buscar un stock por su id de producto")
+    void findStockByProductId(){
+        //When
+        Stock foundStock = stockRepositoryAdapter.findByProductId(productId).orElse(null);
+
+        //Then
+        assertNotNull(foundStock);
+        assertEquals(foundStock.getProductId(), productId);
+    }
+}
