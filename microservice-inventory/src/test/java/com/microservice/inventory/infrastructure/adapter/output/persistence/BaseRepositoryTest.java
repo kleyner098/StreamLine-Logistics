a314e6a6d01a03fc84ad355f021be1733c0e617b
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-04 13:17:24 +0100

    test: add cleanup method to BaseRepositoryTest for database reset

--- microservice-inventory/src/test/java/com/microservice/inventory/infrastructure/adapter/output/persistence/BaseRepositoryTest.java
+++ microservice-inventory/src/test/java/com/microservice/inventory/infrastructure/adapter/output/persistence/BaseRepositoryTest.java
@@ -1,12 +1,22 @@
 package com.microservice.inventory.infrastructure.adapter.output.persistence;
 
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.microservice.inventory.shared.MySQLTestContainer;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class BaseRepositoryTest implements MySQLTestContainer{
+
+    @AfterEach
+    void cleanUp(@Autowired JdbcTemplate jdbcTemplate) {
+        // Esto asegura que cada test empiece con la DB limpia
+        jdbcTemplate.execute("TRUNCATE TABLE products"); 
+        jdbcTemplate.execute("TRUNCATE TABLE stocks"); 
+    }
     
 }
