f0b105c9007da44b125c73f0f797c5e701ed1473
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 23:27:05 +0100

    refactor: enhance documentation for ProductRepositoryAdapter to clarify product repository operations

--- microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Product/ProductRepositoryAdapter.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Product/ProductRepositoryAdapter.java
@@ -9,6 +9,12 @@ import com.microservice.inventory.application.port.output.ProductRepository;
 import com.microservice.inventory.domain.model.Product;
 import com.microservice.inventory.infrastructure.adapter.output.persistence.mappers.ProductEntityMapper;
 
+
+/**
+ * Adaptador para la implementación del repositorio de productos.
+ * Este adaptador utiliza JPA {@link JpaProductRepository} para interactuar con la base de datos MySQL y 
+ * MapStruct {@link ProductEntityMapper} para mapear entre entidades y modelos de dominio.
+ */
 @Component
 public class ProductRepositoryAdapter implements ProductRepository {
 
@@ -20,18 +26,36 @@ public class ProductRepositoryAdapter implements ProductRepository {
         this.productEntityMapper = productEntityMapper;
     }
 
+    /**
+     * Busca un producto por su ID.
+     *
+     * @param id El ID del producto a buscar.
+     * @return Un Optional que contiene el producto si se encuentra, o vacío si no.
+     */
     @Override
     public Optional<Product> findById(Long id) {
         return jpaProductRepository.findById(id)
                 .map(productEntityMapper::toDomain);
     }
 
+    /**
+     * Busca un producto por su SKU.
+     *
+     * @param sku El SKU del producto a buscar.
+     * @return Un Optional que contiene el producto si se encuentra, o vacío si no.
+     */
     @Override
     public Optional<Product> findBySku(String sku) {
         return jpaProductRepository.findBySku(sku)
                 .map(productEntityMapper::toDomain);
     }
 
+    /**
+     * Guarda un producto en la base de datos.
+     *
+     * @param productModel El modelo de dominio del producto a guardar.
+     * @return El producto guardado.
+     */
     @Override
     public Product save(Product productModel) {
         ProductEntity newProductEntity = ProductEntity.builder()
@@ -45,6 +69,11 @@ public class ProductRepositoryAdapter implements ProductRepository {
         return productEntityMapper.toDomain(savedEntity);    
     }
 
+    /**
+     * Encuentra todos los productos.
+     *
+     * @return Una lista de todos los productos.
+     */
     @Override
     public List<Product> findAll() {
         List<ProductEntity> productEntities = jpaProductRepository.findAll();
