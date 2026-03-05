74a728ca9367248dab1bfd206e8542785d82650a
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 23:26:55 +0100

    refactor: enhance documentation for JpaProductRepository to clarify product entity management

--- microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Product/JpaProductRepository.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Product/JpaProductRepository.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repositorio JPA para la gestión de entidades de producto.
+ * Proporciona métodos para acceder a la base de datos y realizar operaciones CRUD.
+ */
 @Repository
 public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
     Optional<ProductEntity> findById(Long id);
