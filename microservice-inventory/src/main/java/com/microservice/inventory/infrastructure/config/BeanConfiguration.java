7079c3c628755233785480fc95e4a46175ae567b
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 23:27:26 +0100

    refactor: enhance documentation for BeanConfiguration to clarify bean definitions and dependencies

--- microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/config/BeanConfiguration.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/config/BeanConfiguration.java
@@ -9,17 +9,39 @@ import com.microservice.inventory.application.port.output.StockRepository;
 import com.microservice.inventory.application.service.CreateProductService;
 import com.microservice.inventory.application.service.GetProductService;
 
+
+/**
+ * Configuración de beans para la inyección de dependencias.
+ * <p>
+ * Dado que la capa de dominio es agnóstica a Spring, este componente se encarga de
+ * instanciar los servicios de aplicación (casos de uso) inyectando sus dependencias
+ * de infraestructura (puertos de salida).
+ */
 @Configuration
 public class BeanConfiguration {
 
+    /**
+     * Bean para el caso de uso de obtener detalles de un producto.
+     * Se inyectan el adaptador de salida para productos {@link ProductRepository} y
+     *  el adaptador de salida para stock {@link StockRepository}, ya que ambos son necesarios
+     * @param productRepository Implementación de persistencia para productos  (MySQL/JPA).
+     * @param stockRepository Implementación de persistencia para stock  (MySQL/JPA).
+     * @return Instancia de {@link GetProductService} que implementa {@link GetProductDetailsUseCase}.
+     */
     @Bean
     public GetProductDetailsUseCase getProductDetailsUseCase(ProductRepository productRepository,
         StockRepository stockRepository) {
-        
-        
         return new GetProductService(productRepository, stockRepository);
     }
 
+    /**
+     * Bean para el caso de uso de crear un producto.
+     * Se inyectan el adaptador de salida para productos {@link ProductRepository} y
+     *  el adaptador de salida para stock {@link StockRepository}, ya que ambos son necesarios
+     * @param productRepository Implementación de persistencia para productos  (MySQL/JPA).
+     * @param stockRepository Implementación de persistencia para stock  (MySQL/JPA).
+     * @return Instancia de {@link CreateProductService} que implementa {@link CreateProductUseCase}.
+     */
     @Bean
     public CreateProductService createProductService(ProductRepository productRepository, StockRepository stockRepository) {
         return new CreateProductService(productRepository, stockRepository);
