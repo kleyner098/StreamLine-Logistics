bc9209a218d9a8a2d921142ce9548e28b7c0fc28
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 22:20:42 +0100

    refactor: enhance documentation for Sku record to clarify its purpose

--- microservice-inventory/src/main/java/com/microservice/inventory/domain/valueobject/Sku.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/domain/valueobject/Sku.java
@@ -2,6 +2,9 @@ package com.microservice.inventory.domain.valueobject;
 
 import com.microservice.inventory.domain.exception.DomainException;
 
+/**
+ * Representa el SKU (Stock Keeping Unit) de un producto.
+ */
 public record Sku(String value) {
     public Sku {
         if (value == null || value.isBlank()) {
