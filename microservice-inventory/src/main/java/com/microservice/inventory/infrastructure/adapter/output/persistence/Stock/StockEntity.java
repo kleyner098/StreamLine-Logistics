c423083c54bdd33db9d77fbffc14a77cd18f8d51
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 23:27:15 +0100

    refactor: enhance documentation for StockEntity to clarify JPA mapping and persistence responsibilities

--- microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Stock/StockEntity.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Stock/StockEntity.java
@@ -12,6 +12,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+/**
+ * Entidad JPA para representar el stock de un producto en la base de datos.
+ * Esta clase se utiliza para mapear los datos del stock a la tabla "stocks" en la base de datos.
+ * No debe contener lógica de negocio, solo atributos y anotaciones para la persistencia.
+ */
 @Entity
 @Table(name = "stocks")
 @Getter
