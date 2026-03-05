a0b3d8033b0cb3d089303ec23db05751fc975887
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 22:53:20 +0100

    refactor: enhance documentation for CreateProductUseCase to clarify product creation process

--- microservice-inventory/src/main/java/com/microservice/inventory/application/port/input/CreateProductUseCase.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/port/input/CreateProductUseCase.java
@@ -4,7 +4,17 @@ import com.microservice.inventory.domain.model.Product;
 import com.microservice.inventory.domain.model.ProductDetails;
 import com.microservice.inventory.domain.model.Stock;
 
+/**
+ * Interfaz para el caso de uso de creación de productos.
+ * Define el método para crear un nuevo producto en el sistema, incluyendo su información y stock inicial.
+ */
 public interface CreateProductUseCase {
 
+    /**
+     * Crea un nuevo producto en el sistema con la información proporcionada y el stock inicial.
+     * @param product
+     * @param stock
+     * @return  Los detalles del producto creado, incluyendo su ID asignado y la información de stock.
+     */
     public ProductDetails createProduct(Product product, Stock stock);
 }
