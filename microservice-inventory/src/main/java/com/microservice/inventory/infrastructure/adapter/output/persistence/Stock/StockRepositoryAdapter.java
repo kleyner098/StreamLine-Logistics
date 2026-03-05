c59318124294ac698c4cff791780dc3ec78efece
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 23:27:20 +0100

    refactor: enhance documentation for StockRepositoryAdapter to clarify stock management operations

--- microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Stock/StockRepositoryAdapter.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Stock/StockRepositoryAdapter.java
@@ -6,6 +6,11 @@ import com.microservice.inventory.application.port.output.StockRepository;
 import com.microservice.inventory.domain.model.Stock;
 import com.microservice.inventory.infrastructure.adapter.output.persistence.mappers.StockEntityMapper;
 
+
+/**
+ * Adaptador de repositorio para la gestión de stock.
+ * Implementa la interfaz {@link StockRepository} y utiliza {@link JpaStockRepository} para acceder a la base de datos MySQL.
+ */
 @Component
 public class StockRepositoryAdapter implements StockRepository{
 
@@ -17,12 +22,25 @@ public class StockRepositoryAdapter implements StockRepository{
         this.stockEntityMapper = stockEntityMapper;
     }
 
+    /**
+     * Busca el stock por el ID del producto.
+     *
+     * @param productId El ID del producto.
+     * @return Un Optional que contiene el stock si se encuentra, o vacío si no.
+     */
     @Override
     public java.util.Optional<com.microservice.inventory.domain.model.Stock> findByProductId(Long productId) {
         return jpaStockRepository.findByProductId(productId)
                 .map(stockEntity -> stockEntityMapper.toDomain(stockEntity));
     }
 
+    /**
+     * Guarda el stock para un producto específico.
+     *
+     * @param productId El ID del producto.
+     * @param stockModel El modelo de dominio del stock a guardar.
+     * @return El stock guardado.
+     */
     @Override
     public Stock save(Long productId, Stock stockModel) {
         StockEntity newStockEntity = StockEntity.builder()
