f6600eb3d6d93a4efb1a7c4c4a2a9ba874d434b5
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 23:27:11 +0100

    refactor: enhance documentation for JpaStockRepository to clarify stock entity management

--- microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Stock/JpaStockRepository.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/infrastructure/adapter/output/persistence/Stock/JpaStockRepository.java
@@ -5,6 +5,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+/**
+ * Repositorio JPA para la gestión de entidades de stock.
+ * Proporciona métodos para acceder a la base de datos y realizar operaciones CRUD.
+ */
 @Repository
 public interface JpaStockRepository extends JpaRepository<StockEntity, Long> {
     Optional<StockEntity> findByProductId(Long productId);
