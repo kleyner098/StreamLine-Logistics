14ad188a16d05696f9ded5701ff7a971ce3c12b4
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 22:57:44 +0100

    refactor: enhance documentation for StockRepository to clarify data access methods

--- microservice-inventory/src/main/java/com/microservice/inventory/application/port/output/StockRepository.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/port/output/StockRepository.java
@@ -4,7 +4,25 @@ import java.util.Optional;
 
 import com.microservice.inventory.domain.model.Stock;
 
+/**
+ * Repositorio para el acceso a los datos de stock.
+ * Define los métodos para interactuar con la capa de persistencia, como buscar el stock por ID de producto, 
+ * guardar o actualizar el stock, etc.
+ */
 public interface StockRepository {
+
+    /**
+     * Busca el stock de un producto por su ID.
+     * @param productId
+     * @return Un Optional que contiene el stock si se encuentra, o vacío si no existe.
+     */
     Optional<Stock> findByProductId(Long productId);
+
+    /**
+     * Guarda o actualiza el stock de un producto.
+     * @param productId
+     * @param stock
+     * @return El stock guardado o actualizado.
+     */
     Stock save(Long productId, Stock stock);
 }
