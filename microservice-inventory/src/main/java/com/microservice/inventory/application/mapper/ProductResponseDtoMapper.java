8fb768b78f1396781beff15aecddec108f6c33ec
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-05 22:53:10 +0100

    refactor: enhance documentation for ProductResponseDtoMapper to clarify mapping behavior

--- microservice-inventory/src/main/java/com/microservice/inventory/application/mapper/ProductResponseDtoMapper.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/application/mapper/ProductResponseDtoMapper.java
@@ -10,12 +10,19 @@ import org.mapstruct.NullValueMappingStrategy;
 import com.microservice.inventory.application.dto.ProductReponseDto;
 import com.microservice.inventory.domain.model.ProductDetails;
 
+
+/**
+ * Mapper para convertir entre ProductDetails y ProductResponseDto.
+ * Utiliza MapStruct para generar el código de mapeo automáticamente.
+ * Configurado para manejar valores nulos de manera segura.
+ */
 @Mapper(componentModel = "spring",
     nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
     nullValueIterableMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT
 )
 public interface ProductResponseDtoMapper {
 
+    
     @Mapping(target = "sku", source = "product.sku.value") 
     @Mapping(target = "name", source = "product.name")
     @Mapping(target = "description", source = "product.description")
@@ -25,6 +32,11 @@ public interface ProductResponseDtoMapper {
     ProductReponseDto toResponse(ProductDetails details);
     
     //Si el stock existe, llama al método de negocio available() que se definio en la clase Stock
+    /**
+     * Mapea el stock disponible de un producto a partir de los detalles del producto.
+     * @param details
+     * @return El stock disponible del producto, o 0 si el stock es nulo.
+     */
     default Integer mapAvailableStock(ProductDetails details) {
         if (details.stock() == null) return 0;
         return details.stock().available();
