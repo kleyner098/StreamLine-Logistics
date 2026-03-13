d77dfcbd8d8b13e096bc2cf260eeed3b9ff6939b
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-13 23:38:55 +0100

    feat: refactor Order class to remove status parameter and add item management methods

--- microservice-order/src/main/java/com/microservice/order/domain/model/Order.java
+++ microservice-order/src/main/java/com/microservice/order/domain/model/Order.java
@@ -1,6 +1,9 @@
 package com.microservice.order.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import com.microservice.order.domain.exception.DomainException;
 
@@ -11,25 +14,36 @@ public class Order {
     private final Long customerId;
     private final Status status;
     private final BigDecimal totalAmount;
+    private final List<OrderItem> items = new ArrayList<>();
 
 
-    public Order(Long id, String orderNumber, Long customerId, Status status, BigDecimal totalAmount) {
+    public Order(Long id, String orderNumber, Long customerId, BigDecimal totalAmount) {
         
         validateOrderNumber(orderNumber);
         validateCustomerId(customerId);
         validateTotalAmount(totalAmount);
-        
+
         this.id = id;
         this.orderNumber = orderNumber;
         this.customerId = customerId;
-        this.status = status;
+        this.status = Status.PENDING;
         this.totalAmount = totalAmount;
     }
 
     // Métodos de cambio de estado (Inmutabilidad)
 
-    public Order updateStatus(Status newStatus) {
-        return new Order(this.id, this.orderNumber, this.customerId, newStatus, this.totalAmount);
+    public void addItem(OrderItem item) {
+        if (this.status != Status.PENDING) {
+            throw new IllegalStateException("No se pueden añadir ítems si el pedido no está pendiente.");
+        }
+        this.items.add(item);
+    }
+
+    public void removeItem(OrderItem item) {
+        if (this.status != Status.PENDING) {
+            throw new IllegalStateException("No se pueden eliminar ítems si el pedido no está pendiente.");
+        }
+        this.items.remove(item);
     }
 
     // Validaciones 
@@ -58,5 +72,5 @@ public class Order {
     public Long getCustomerId() {return customerId;}
     public Status getStatus() {return status;}
     public BigDecimal getTotalAmount() {return totalAmount;}
-    
+    public List<OrderItem> getItems() {return Collections.unmodifiableList(items);}
 }
