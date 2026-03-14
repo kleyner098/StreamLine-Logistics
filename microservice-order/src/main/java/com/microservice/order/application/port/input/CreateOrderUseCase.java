d5d128a66533c6356d87667615dfce970be28a9e
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-14 10:20:05 +0100

    feat: add createOrder method to CreateOrderUseCase interface for order creation

--- microservice-order/src/main/java/com/microservice/order/application/port/input/CreateOrderUseCase.java
+++ microservice-order/src/main/java/com/microservice/order/application/port/input/CreateOrderUseCase.java
@@ -1,6 +1,10 @@
 package com.microservice.order.application.port.input;
 
+import com.microservice.order.domain.model.OrderItem;
+
 public interface CreateOrderUseCase {
 
+    
+    public Long createOrder(String customerId, OrderItem item);
     
 }
