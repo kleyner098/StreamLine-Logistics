20369258b64fcc12b165dc93b3861a793ec86016
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-15 12:02:26 +0100

    feat: add OrdenItemEntity class for order item persistence

---
+++ microservice-order/src/main/java/com/microservice/order/infrastructure/adapter/output/persistance/orderItem/OrdenItemEntity.java
@@ -0,0 +1,42 @@
+package com.microservice.order.infrastructure.adapter.output.persistance.orderItem;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrdenItemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    @Column(name = "price_at_purchase", precision = 10, scale = 2)
+    private BigDecimal priceAtPurchase;
+
+}
