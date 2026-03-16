3f91c0635845db096d50937c85cacc069d575d69
kleyner098 <kleyner_ecua@hotmail.com>
2026-03-16 21:33:53 +0100

    feat: add Javadoc comments to CreateOrderUseCase interface

--- microservice-order/src/main/java/com/microservice/order/application/port/input/CreateOrderUseCase.java
+++ microservice-order/src/main/java/com/microservice/order/application/port/input/CreateOrderUseCase.java
@@ -2,6 +2,11 @@ package com.microservice.order.application.port.input;
 
 import com.microservice.order.domain.model.Order;
 
+/**
+ * Interfaz para el caso de uso de creación de orden.
+ * Define el contrato para la creación de una orden, que incluye la información de la orden y sus items asociados.
+ * Esta interfaz es implementada por el servicio de aplicación CreateOrderService, que se encarga
+ */
 public interface CreateOrderUseCase {
 
     
